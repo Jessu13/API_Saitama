7d461ba9f3d5d5394d6bc2d829aa4f88b8489904
Jessuar Hoyos <jessuarhandan@gmail.com>
2022-10-30 20:06:13 -0500

    AuthLayout

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-        <h1>Desde Auth Layout</h1>
+        <h1 className="font-bold text-center">Desde Auth Layout</h1>
 
         <Outlet/>
     </>
