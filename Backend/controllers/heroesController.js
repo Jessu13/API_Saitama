93ddf9ffd4f6170374e596c9e0b1b7e32e40ec2a
pipeCano23 <fecamo2002@gmail.com>
2022-11-01 18:45:52 -0500

    Create heroesController.js

---
+++ Backend/controllers/heroesController.js
@@ -0,0 +1,43 @@
+import { where } from "sequelize";
+import db from "../configuracion/db.js";
+import { Heroe } from "../models/heroes.js";
+
+//GET
+const encontrar_Heroes = async (req, res) => {
+    try{
+        const heroes_claseS = await Heroe.findAll({
+            where: {
+                rango: "S"
+            }
+        });
+        const heroes_claseA = await Heroe.findAll({
+            where: {
+                rango: "A"
+            }
+        });
+        const heroes_claseB = await Heroe.findAll({
+            where: {
+                rango: "B"
+            }
+        });
+        const heroes_claseC = await Heroe.findAll({
+            where: {
+                rango: "C"
+            }
+        });
+        heroes = {heroes_claseS, heroes_claseA, heroes_claseB, heroes_claseC}
+        res.json(heroes)
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
+// GET
+const top10Heroes = async (req, res) => {
+    try {
+        // COMPLETAR
+        const top10 = await db.query("SELECT * FROM heroes")
+        res.json(top10)
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
