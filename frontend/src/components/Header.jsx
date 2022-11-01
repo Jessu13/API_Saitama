3f4240d6de0daabebaca21f80ab404fc6b63f2aa
pipeCano23 <fecamo2002@gmail.com>
2022-11-01 18:21:32 -0500

    color prueba

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
       <>
-          <header className="py-10 bg-indigo-600">
+          <header className="py-10 bg-slate-400">
               <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
                   <h1 className="font-bold text-2xl text-indigo-200">Aministrador de Monstruos</h1>
               </div>
--- frontend/src/paginas/AdministrarMonstruos.jsx
+++ frontend/src/paginas/AdministrarMonstruos.jsx
@@ -9,7 +9,7 @@ const AdministrarMonstruos = () => {
   return (
     <>
         <div className="flex flex-col md:flex-row">
-          <button type="button" className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden" onClick={() => setMostrarFormulario(!mostrarFormulario)}>
+          <button type="button" className="bg-slate-400 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden" onClick={() => setMostrarFormulario(!mostrarFormulario)}>
             {mostrarFormulario ? 'Ocultar Formulario' : 'Mostrar Formulario'}
           </button>
           <div className={`${mostrarFormulario ? 'block' : 'hidden' } md:block md:2-1/2 lg:w-2/5`}>
