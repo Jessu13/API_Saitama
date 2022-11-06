5ff65039c9ac86231424390db6b1e01b2307e746
pipeCano23 <fecamo2002@gmail.com>
2022-11-05 20:44:17 -0500

    Patrocinadores linea
    Corregir el envio del JSON, enviar en un solo object el nombre del patrocinador y todos los heroes que patrocina

--- Backend/controllers/patrocinadoresController.js
+++ Backend/controllers/patrocinadoresController.js
@@ -5,10 +5,28 @@ const get_Patrocinadores = async (req, res) => {
 
     const [patrocinadores, metadata] = await db.query("SELECT patrocinador.nombre_patrocinador, heroes.nombre_heroe from heroes, patrocinador, patrocinador_heroe where patrocinador_heroe.id_heroe=heroes.id and patrocinador_heroe.id_patrocinador=patrocinador.id order by patrocinador.nombre_patrocinador");
 
-    console.log(patrocinadores)
+    if(patrocinadores.length > 0){
+        var patrocinadores_json = [];
+        var patrocinador = {
+            nombre_patrocinador: patrocinadores[0].nombre_patrocinador,
+            heroes: patrocinadores[0].nombre_heroe
+        }
+        for (let index = 1; index < patrocinadores.length; index++) {
+            if(patrocinadores[index - 1].nombre_heroe == patrocinadores[index].nombre_heroe){
+                patrocinador.heroes += ", " + patrocinadores[index].nombre_heroe;
+            } else {
+                patrocinadores_json.push(patrocinador);
+                patrocinador = {
+                    nombre_patrocinador: patrocinadores[index].nombre_patrocinador,
+                    heroes: patrocinadores[index].nombre_heroe
+                }
+            }
+        }
+        patrocinadores_json.push(patrocinador);
+    }
 
     try{
-        res.json(patrocinadores)
+        res.json(patrocinadores_json)
     } catch (error) {
         console.log(error);
     }
