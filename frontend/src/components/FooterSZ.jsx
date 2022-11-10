caa594db37f77b44b494b3f6d060f2de1fae35fc
pipeCano23 <fecamo2002@gmail.com>
2022-11-10 15:35:40 -0500

    Creacion paginas
    Creacion pagina de saitamazone e inicial, ademas se agrego todas las rutas a los headers

---
+++ frontend/src/components/FooterSZ.jsx
@@ -0,0 +1,11 @@
+const FooterSZ = () => {
+    return (
+        <>
+            <footer className="py-10">
+                <p className="text-center font-bold mt-auto">Administración de Saitama Zone en la API {''} <span className="text-indigo-600">Saitama</span></p>
+            </footer>
+        </>
+    )
+}
+
+export default FooterSZ
---
+++ frontend/src/paginas/AdministrarSaitamaZone.jsx
@@ -0,0 +1,11 @@
+import siatama from '../assets/SZ_Front.png'
+
+const PaginaSZ = () => {
+    return (
+        <>
+        <img style={{width: "50%", height:"50%"}} src={siatama} alt="Saitama" className='mx-auto' />
+            </>
+    )
+}
+
+export default PaginaSZ
